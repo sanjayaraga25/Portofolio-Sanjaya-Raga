e8667d94aade485a4d1cbd6f48a5758a63c86fd5
sanjayaraga25 <116234477+sanjayaraga25@users.noreply.github.com>
2026-05-28 11:55:54 +0700

    Fix trustProxies config, remove wrong constants

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('/admin/login');
 
-        $middleware->trustProxies(at: '*', headers: TrustProxies::HEADER_X_FORWARDED_FOR | TrustProxies::HEADER_X_FORWARDED_HOST | TrustProxies::HEADER_X_FORWARDED_PORT | TrustProxies::HEADER_X_FORWARDED_PROTO);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
